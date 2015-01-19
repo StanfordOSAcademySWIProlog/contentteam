a742a8e3f749fabfede6620c9a26e6e105c0c415
Jxliu <xiaoyu.justin.liu@gmail.com>
2015-01-18 22:46:45 -0800

    update

--- backend.pl
+++ backend.pl
@@ -1,8 +1,10 @@
 :- module(backend,[data/1]).
 
-:- use_module(db,[course/1]).
+:- use_module(db,[course/4]).
 
 data(Courses) :-
-    findall(C, course(C), Courses).
+    findall(major_nr_title_units(M, N, T, U), course(M, N, T, U), Courses).
 
+
+  
 
