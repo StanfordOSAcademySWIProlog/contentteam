efad29eac018b009e097f8bd7ba707e1f5631afd
Boris Vassilev <boris.vassilev@gmail.com>
2015-01-31 09:52:11 +0200

    Added a way for dealing with prereqs as boolean expressions

--- backend.pl
+++ backend.pl
@@ -21,3 +21,32 @@ course(ID, Title, Units, Descr, Reqs) :-
     t_course_descr(ID, Descr),
     t_course_reqs(ID, Reqs).
 
+
+% Represent prerequisites as a "boolean" expression
+% - An operand (value) is the term `val(Op)`
+% - An AND is the term `and(X, Y)`
+% - An OR is the term `or(X, Y)`
+% Examples:
+% - (a AND b AND c):
+%     and(val(a), and(val(b), val(c)))
+%       OR
+%     and(and(val(a), val(b)), val(c))
+% - (a OR (b AND c)):
+%     or(val(a), and(val(b), val(c)))
+% - (a AND b AND (c OR d)) OR (f AND g):
+%     or(and(val(a), and(val(b), or(val(c), val(d)))), and(val(f), val(g)))
+% 
+% ... and so on.
+%
+% Convert the boolean expresssions to lists,
+% enumerating the different possibilities by backtracking
+%
+bool_to_list(none, Rest, Rest).
+bool_to_list(val(X), [X|Rest], Rest).
+bool_to_list(and(X, Y), L0, L1) :-
+    bool_to_list(X, L0, L),
+    bool_to_list(Y, L, L1).
+bool_to_list(or(X, Y), L0, L1) :-
+    (   bool_to_list(X, L0, L1)
+    ;   bool_to_list(Y, L0, L1)
+    ).
