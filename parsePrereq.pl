dc6ebeaecacff0d81b632a007a2713897d3d92d1
Kristiyan Dzhamalov <kdzhamal@ucsd.edu>
2015-02-17 23:11:53 -0800

    Adding functionality for parsing the prerequisites for a course or list of courses. API design stage. Advancing to parsing course prereqs stage

---
+++ parsePrereq.pl
@@ -0,0 +1,41 @@
+:- module(parse_prereqs, [parse_a_class_prereqs/2, parse_multi_class_prereqs/2]).
+:- license(lgpl).
+
+:- use_module(get_all_links, [get_all_prereq_links/3, get_a_prereq_link/3]).
+
+:- use_module(library(http/http_ssl_plugin)).
+:- use_module(library(http/http_open)).
+:- use_module(library(xpath)).
+
+
+%%	Obtains all prerequisites for a specific course.
+% CourseID is expected to be a courseID.
+% Prereqs is [ ['CSE 11', 'or', 'CSE 8B'], ['CSE XXX']].
+parse_a_class_prereqs(CourseID, Prereqs) :-
+	once(get_a_prereq_link("WI14", CourseID, [URL|_])),
+	scrape(URL, Prereqs).
+
+%%	Obtains all prerequisites for a list of courses.
+% CourseIDs is a list of courses that contains the names of the courses
+% as atoms.
+% Prereqs is a list of lists of courseID and list of prerequisites.
+% i.e. [ [ 'CSE 12', [ ['CSE 11', 'or', 'CSE 8B'], [ 'CSE XXX']]], ..].
+
+parse_multi_class_prereqs(CourseIDs, Prereqs) :-
+	parse_list(CourseIDs, Prereqs).
+
+scrape(URL, Data) :-
+    setup_call_cleanup(http_open(URL, In, []),
+        scrape_stream(In, Data),
+        close(In)).
+
+scrape_stream(In, Data) :-
+    load_html(In, DOM, [syntax_errors(quiet)]),
+    prereqs(DOM, Data).
+
+parse_list([],[]).
+parse_list([Course | T], [[Course | ListOfPrereqs]|ParsePrereqs]) :-
+	parse_a_class_prereqs(Course, ListOfPrereqs),
+	parse_list(T, ParsePrereqs).
+
+prereqs(DOM, Prereqs) :- true.
