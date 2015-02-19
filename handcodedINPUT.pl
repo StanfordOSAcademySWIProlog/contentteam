ffcd372cba006bb94686a60cbf7c79bb71c20f8f
Jxliu <xiaoyu.justin.liu@gmail.com>
2015-02-18 20:54:49 -0800

    added handcoded input which comes from front end; fixed tech_tree bug

---
+++ handcodedINPUT.pl
@@ -0,0 +1,5 @@
+:- module(handcodedINPUT, [course_taken/2]).
+
+course_taken('CSE 12').
+course_taken('CSE 20').
+course_taken('MATH 109').
--- tech_tree.pl
+++ tech_tree.pl
@@ -1,51 +1,39 @@
 % TechTree 
 
-:- module(tech_tree).
+:- module(tech_tree, [prereq_tree/1]).
 :- use_module(library(ugraphs)).
 
 % for now
 :- use_module(handcodedDB, [major/2, course/5, requirement/3]).
+:- use_module(handcodedINPUT, [course_taken/1]).
 :- use_module(prereq_proc, [requirement_to_list/3, requirement_to_string/3]).
 
 
 course_list(L) :-
     findall(X, course(X, _, _, _ ,_), L).
 
+% get from front-end
 course_taken_list(L) :-
-    findall(X, course_taken(X), L). 
+   findall(X, course_taken(X), L). 
 
 course_remain_list(L) :-
     course_list(X),
     course_taken_list(Y),
     subtract(X, Y, L). 
 
-
 % X is a pre-req of Y
+% use with caution, is_prereq(X, 'CSE 100') => X will contain duplicates
 is_prereq(X, Y) :-
     dif(X, Y),
-    course(X, _, _, _, _),
     requirement_to_list(Dep,ID,L),
-    atom_concat(Dep, ID, Y),
+    atom_concat(Dep, ' ', T),
+    atom_concat(T, ID, Y),
     member(X, L).
 
-
-prereq_tree_min(Dep, ID, T) :-
-    requirement_to_list(Dep, ID, L),
-
-
-
-
-
-% Tree contanis ALL courses
+% Tree contanis ALL courses in the db
 prereq_tree(T) :-
     course_list(Vertices),
     findall(Y-X, is_prereq(X, Y), Edges),
     vertices_edges_to_ugraph(Vertices, Edges, T).
-
-% simple order without course_taken
-order(L) :-
-    prereq_tree(G),
-    course_goal(J),
-    reachable(J, G, L).
 
     
