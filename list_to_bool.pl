5061fe62d6a5b61176eccbdae4da368e9de20d6c
Yuxiang_Di <ydi@ucsd.edu>
2015-02-27 03:00:26 -0800

    list_to_bool is finished and tested, ready to clean up and merge to master

--- list_to_bool.pl
+++ list_to_bool.pl
@@ -7,8 +7,38 @@ sample('2',[['CSE 12'],['CSE 11','or','CSE 8B']]).
 sample('3',[['CSE 12'],['CSE 11','or','CSE 8B'],['CSE 15L']]).
 sample('4',[['CSE 12'],['CSE 11','or','CSE 8B','or','CSE 190'],['CSE 15L']]).
 
+test(D,R):-sample(D,L),list_to_bool(L,R).
 
-list_to_bool(L,bool(B)).
+
+
+
+:- use_module(library(apply)).
+%main function:
+%convert a list of list of courses into boolean expression
+%the inner list are in OR relation and outer are AND and relation
+%inner list will contain 'or' between each courses
+%exapmple:
+%[['CSE 12'],['CSE 11']] ->  bool(and(val(id('CSE','11')),val(id('CSE','12'))))
+%
+%[['CSE 12'],['CSE 11','or','CSE 8B','or','CSE 190'],['CSE 15L']]->
+% bool(and(and(val(id('CSE', '15L')), or(or(val(id('CSE', '190')),
+% val(id('CSE', '8B'))), val(id('CSE', '11')))), val(id('CSE', '12'))))
+%
+%
+%boolean expression can be read by:
+% use_module(prereq_proc,[requirement_to_list/3,requirement_to_string/3]).
+list_to_bool(L,bool(B)):-
+	list_to_valId_list(L,VL),
+        maplist(orlist_to_bool,VL,OrList),
+	andlist_to_bool(OrList,B).
+
+
+
+%convert a list of list of courses into val(id(D,N)) list list.
+list_to_valId_list([],[]).
+list_to_valId_list([H|T],[H1|T1]):-
+	maplist(course_to_valId,H,H1),
+	list_to_valId_list(T,T1).
 
 
 %convert list['1','2','3'] into and(and('3','2'),'1')
@@ -41,8 +71,9 @@ course_to_id(C,ID):-
 	phrase(course_phraser(ID), Codes).
 
 % C='CSE 12' -> ID= val(id('CSE','12))
-course_to_val_id(C,val(ID)):-
-	course_to_id(C,ID).
+course_to_valId('or','or'):-!.
+course_to_valId(C,val(ID)):-
+	course_to_id(C,ID),!.
 
 
 :- use_module(library(dcg/basics)).
