9d1a8603e62ead165e9f9c3b262f1f8d492fe792
Yuxiang_Di <ydi@ucsd.edu>
2015-03-07 13:41:30 -0800

    add option for orlist without 'or' between each class

--- list_to_bool.pl
+++ list_to_bool.pl
@@ -5,7 +5,8 @@
 sample('1',[['CSE 12'],['CSE 11']]).
 sample('2',[['CSE 12'],['CSE 11','or','CSE 8B']]).
 sample('3',[['CSE 12'],['CSE 11','or','CSE 8B'],['CSE 15L']]).
-sample('4',[['CSE 12'],['CSE 11','or','CSE 8B','or','CSE 190'],['CSE 15L']]).
+%#4 are format without 'or' in OR LIST
+sample('4',[['CSE 12'],['CSE 11','CSE 8B','CSE 190'],['CSE 15L']]).
 
 test(D,R):-sample(D,L),list_to_bool(L,R).
 
@@ -16,9 +17,10 @@ test(D,R):-sample(D,L),list_to_bool(L,R).
 %main function:
 %convert a list of list of courses into boolean expression
 %the inner list are in OR relation and outer are AND and relation
-%inner list will contain 'or' between each courses
-%exapmple:
-%[['CSE 12'],['CSE 11']] ->  bool(and(val(id('CSE','11')),val(id('CSE','12'))))
+% inner list may contain 'or' between each courses or not depend on
+% choice of use orlist_to_bool_noOR or orlist_to_bool_withOR exapmple:
+% [['CSE 12'],['CSE 11']] ->
+% bool(and(val(id('CSE','11')),val(id('CSE','12'))))
 %
 %[['CSE 12'],['CSE 11','or','CSE 8B','or','CSE 190'],['CSE 15L']]->
 % bool(and(and(val(id('CSE', '15L')), or(or(val(id('CSE', '190')),
@@ -29,7 +31,7 @@ test(D,R):-sample(D,L),list_to_bool(L,R).
 % use_module(prereq_proc,[requirement_to_list/3,requirement_to_string/3]).
 list_to_bool(L,bool(B)):-
 	list_to_valId_list(L,VL),
-        maplist(orlist_to_bool,VL,OrList),
+        maplist(orlist_to_bool_noOR,VL,OrList),
 	andlist_to_bool(OrList,B).
 
 
@@ -47,17 +49,22 @@ andlist_to_bool([],'').
 andlist_to_bool([H|[]],H):-!.
 andlist_to_bool([H|T],and(B,H)):-andlist_to_bool(T,B).
 
+%convert list ['1','2','3'] into or(or('3','2'),'1')
+orlist_to_bool_noOR([],'').
+orlist_to_bool_noOR([H|[]],H):-!.
+orlist_to_bool_noOR([H|T],or(B,H)):-orlist_to_bool_noOR(T,B).
+
 %convert list ['1','or','2','or','3'] into or(or('3','2'),'1')
 %it return false if format does not match above.
-orlist_to_bool([],'').
-orlist_to_bool([H|[]],H):-
+orlist_to_bool_withOR([],'').
+orlist_to_bool_withOR([H|[]],H):-
 	not(H='or'),!.
-orlist_to_bool([H|T],or(B,H)):-
+orlist_to_bool_withOR([H|T],or(B,H)):-
 	not(H='or'),
-	orlist_to_bool2(T,B).
-orlist_to_bool2(['or'|[]],_):-!,false.
-orlist_to_bool2(['or'|T],B):-
-	orlist_to_bool(T,B).
+	orlist_to_bool_withOR2(T,B).
+orlist_to_bool_withOR2(['or'|[]],_):-!,false.
+orlist_to_bool_withOR2(['or'|T],B):-
+	orlist_to_bool_withOR2(T,B).
 
 
 
